381a033737f986243293224dc49c55125687c310
Jose Mayoral <jocamaba1989@gmail.com>
2018-08-06 18:57:08 +0200

    fixing issues with xml structure

--- ros_plugins.py
+++ ros_plugins.py
@@ -5,7 +5,7 @@ import re
 
 rospy.init_node("dynamic_reconfigure_planners")
 
-dyn_client = Client("move_base", None)
+dyn_client = Client("/move_base", None)
 
 
 plugins = os.popen("rospack plugins --attrib=plugin nav_core").read()
@@ -15,23 +15,58 @@ plugin_name_type = list()
 for i in plugins:
     j = i.split()
     c_file = open(j[1],"r")
+    flags = [False,False,False]
     for line in c_file:
         index_name = line.find("name")
         index_type = line.find("type")
         base_class_index = line.find("base_class_type")
 
-        if index_name > 0:
+        if index_name > 0 and not flags[0]:
             name = re.findall(r'"(.*?)"', line[index_name:])[0]
-            if index_type>0:
-                plugin_type = re.findall(r'"(.*?)"', line[index_type:])[0]
-                if base_class_index > 0:
-                    plugin_base_type = re.findall(r'"(.*?)"', line[base_class_index:])[0]
-                    plugin_name_type.append([name, plugin_type, plugin_base_type])
+            flags[0] = True
+        if index_type>0 and not flags[1]:
+            plugin_type = re.findall(r'"(.*?)"', line[index_type:])[0]
+            flags[1] = True
+        if base_class_index > 0 and not flags[2]:
+            plugin_base_type = re.findall(r'"(.*?)"', line[base_class_index:])[0]
+            flags[2] = True
+        
+        if all(flags):
+            plugin_name_type.append([name, plugin_type, plugin_base_type])
+            flags = [False,False,False]
+
+available_global_planners = list()
+available_local_planners = list()
 
 for n_t in plugin_name_type:
+    print n_t[0], n_t[2]
     if n_t[2] == "nav_core::BaseGlobalPlanner":
-        print "name ", n_t[0] , " type " , n_t[1], "Base Type" , n_t[2]
-        dyn_client.update_configuration({"global_planner": n_t[0]})
-        break
+        available_global_planners.append(n_t[0])
+
+    if n_t[2] == "nav_core::BaseLocalPlanner":
+        available_local_planners.append(n_t[0])
+
+
+new_config = dict()
+new_config["global_planner"] = "Error"
+new_config["local_planner"] = "Error"
+
+print "Select Your Global Planner"
+
+for i in range(len(available_global_planners)):
+    print "Available GP ", i , available_global_planners[i]
+
+new_config["global_planner"] = available_global_planners[int(raw_input('Choose a number: '))]
+
+print "Select Your Local Planner"
+
+for i in range(len(available_local_planners)):
+    print "Available LP ", i , available_local_planners[i]
+
+new_config["local_planner"] = available_local_planners[int(raw_input('Choose a number: '))]
+
+print new_config
+dyn_client.update_configuration(new_config)
+#break
 
 print "finish"
