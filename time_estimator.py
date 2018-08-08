eec807bff43aad84d524b55bcfa93aa06aa03870
Jose Mayoral <jocamaba1989@gmail.com>
2018-08-08 13:59:07 +0200

    Revert "simplifying script"
    This reverts commit 23c87d754bde0c430e715aa493e8e77f4cd02e13.

--- time_estimator.py
+++ time_estimator.py
@@ -5,6 +5,7 @@ from std_msgs.msg import Empty, Bool
 class TimeEstimator:
     def __init__(self):
         rospy.init_node("time_estimator")
+        self.accumulator = 0
         self.samples = 0
         self.is_training = True
         rospy.Subscriber("/global_planner/plan", Path, self.PathCB, queue_size=1)
@@ -36,7 +37,7 @@ class TimeEstimator:
         self.K = self.K/len(msg.poses)
         #print "Curvature " , self.K 
         if self.samples >0:# not self.is_training:
-            self.estimated_time = self.estimated_time - (self.weight/self.samples)*self.lenght + self.K / self.weight
+            self.estimated_time = self.estimated_time - (self.accumulator/self.samples)*self.lenght + self.K / self.weight
         print "Estimated TIME ", self.estimated_time
 
     def MotionCompleteCB(self,msg):
@@ -44,7 +45,8 @@ class TimeEstimator:
 
         if self.is_training:
             self.samples = self.samples+1
-            self.weight += (self.estimated_time - measured_time)/self.lenght
+            self.accumulator += (self.estimated_time - measured_time)/self.lenght
+            self.weight += (self.estimated_time - measured_time)
 
         print "MEASURED TIME", measured_time
         print "ERROR IN SECONDS", self.estimated_time - measured_time
