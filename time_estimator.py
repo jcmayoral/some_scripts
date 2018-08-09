449cff4021be1ff0d9f2bc9611c8cf660a731245
Jose Mayoral <jocamaba1989@gmail.com>
2018-08-09 17:53:26 +0200

    second derivative for curvature

--- time_estimator.py
+++ time_estimator.py
@@ -1,6 +1,8 @@
 import rospy
 from nav_msgs.msg import Path
 from std_msgs.msg import Empty, Bool
+from geometry_msgs.msg import Vector3
+import numpy as np
 
 class TimeEstimator:
     def __init__(self):
@@ -11,6 +13,7 @@ class TimeEstimator:
         rospy.Subscriber("/global_planner/plan", Path, self.PathCB, queue_size=1)
         rospy.Subscriber("/local_planner/isGoalReached", Empty, self.MotionCompleteCB, queue_size=1)
         rospy.Subscriber("/time_estimator/training", Bool, self.trainingCB, queue_size=1)
+        self.feedback_pub = rospy.Publisher("time_estimator/fb", Vector3)
         self.start_time = rospy.Time.now()
         self.estimated_time = 0
         self.lenght = 1
@@ -20,24 +23,48 @@ class TimeEstimator:
 
     def trainingCB(self,msg):
         self.is_training = msg.data
+        if not self.is_training:
+            print "OFF SET per pixel is ", self.accumulator/self.samples
 
     def PathCB(self,msg):
         self.lenght = len(msg.poses)
         self.estimated_time = 0.1*len(msg.poses)
         self.start_time = rospy.Time.now()
-        #print "Estimated TIME ", self.estimated_time
 
         p0 = msg.poses[0]
         self.K = 0
 
-        for p in msg.poses[1:]:
-            self.K = self.K + (p.pose.position.x -p0.pose.position.x) 
-            self.K = self.K + (p.pose.position.y -p0.pose.position.y) 
+        dx = list()
+        dy = list()
+        ddx = list()
+        ddy = list()
 
-        self.K = self.K/len(msg.poses)
+        for p in msg.poses[1:]:
+            dx.append(p.pose.position.x - p0.pose.position.x)
+            dy.append(p.pose.position.y - p0.pose.position.y)
+            p0 = p
+
+        dx0 = dx[0]
+        dy0 = dy[0]
+
+        for x,y in zip(dx[1:],dy[1:]):
+            ddx.append(x - dx0)
+            ddy.append(y - dy0)
+            dx0 = x
+            dy0 = y
+
+        dx = np.sum(dx,axis=0)
+        dy = np.sum(dy,axis=0)
+        ddx = np.sum(ddx, axis=0)
+        ddy = np.sum(ddy,axis=0)
+
+        self.K = (ddy * dx - ddx * dy) / (np.power(dx, 2) + np.power(dy, 2))
+        #self.K/=self.lenght
+
         #print "Curvature " , self.K 
+
         if self.samples >0:# not self.is_training:
-            self.estimated_time = self.estimated_time - (self.accumulator/self.samples)*self.lenght + self.K / self.weight
+            self.estimated_time = self.estimated_time - (self.accumulator/self.samples)*self.lenght + self.K * self.lenght / self.weight
         print "Estimated TIME ", self.estimated_time
 
     def MotionCompleteCB(self,msg):
@@ -49,7 +76,13 @@ class TimeEstimator:
             self.weight += (self.estimated_time - measured_time)
 
         print "MEASURED TIME", measured_time
-        print "ERROR IN SECONDS", self.estimated_time - measured_time
+        print "Percentage", (measured_time- self.estimated_time)/measured_time
+
+        fb_msgs = Vector3()
+        fb_msgs.x = np.fabs(measured_time - self.estimated_time)/measured_time
+        fb_msgs.y = self.lenght
+        fb_msgs.z = self.K
+        self.feedback_pub.publish(fb_msgs)
         #print "ERROR per primitive ", (self.estimated_time - measured_time)/self.lenght
 
 TimeEstimator()
