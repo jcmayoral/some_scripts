ddd01e598ae4ed36eb5293aa27a8d54c7f884765
Jose Mayoral <jocamaba1989@gmail.com>
2018-08-14 10:07:48 +0200

    time estimator working

--- navigation/time_estimator.py
+++ navigation/time_estimator.py
@@ -7,17 +7,16 @@ import numpy as np
 class TimeEstimator:
     def __init__(self):
         rospy.init_node("time_estimator")
-        self.accumulator = 0
+        self.mean_primitive_error = 0
         self.samples = 0
         self.is_training = True
-        rospy.Subscriber("/global_planner/plan", Path, self.PathCB, queue_size=1)
-        rospy.Subscriber("/local_planner/isGoalReached", Empty, self.MotionCompleteCB, queue_size=1)
+        rospy.Subscriber("/navigation/move_base_flex/SBPLLatticePlanner/plan", Path, self.PathCB, queue_size=1)
+        rospy.Subscriber("/navigation/move_base_flex/PoseFollower/motion_finished", Empty, self.MotionCompleteCB, queue_size=1)
         rospy.Subscriber("/time_estimator/training", Bool, self.trainingCB, queue_size=1)
         self.feedback_pub = rospy.Publisher("time_estimator/fb", Vector3)
         self.start_time = rospy.Time.now()
         self.estimated_time = 0
         self.lenght = 1
-        self.weight = 0
         self.K =0
         self.A = list()
         self.y = list()
@@ -27,15 +26,17 @@ class TimeEstimator:
     def trainingCB(self,msg):
         self.is_training = msg.data
         if not self.is_training:
-            print "OFF SET per pixel is ", self.accumulator/self.samples
+            print "OFF SET per primitive is ", self.mean_primitive_error/self.samples
         
-        arr = self.accumulator/self.samples * np.ones(len(self.y))
-        self.C = np.vstack([self.A, arr]).T
-        self.coefficients = np.linalg.lstsq(self.C, self.y)[0]
+        #offset = np.ones(len(self.y))
+        #self.C = np.vstack([self.A, offset]).T
+        self.coefficients = np.linalg.lstsq(self.A, self.y)[0]
+        print self.coefficients
 
     def PathCB(self,msg):
         self.lenght = len(msg.poses)
         self.estimated_time = 0.1*len(msg.poses)
+        print "ORIGINAL ESTIMATION" , self.estimated_time
         self.start_time = rospy.Time.now()
 
         p0 = msg.poses[0]
@@ -68,24 +69,25 @@ class TimeEstimator:
         self.K = (ddy * dx - ddx * dy) / (np.power(dx, 2) + np.power(dy, 2)) 
         #print "Curvature " , self.K 
 
-        if self.samples >0:# not self.is_training:
-            if self.coefficients is not None:
-                self.estimated_time = self.estimated_time + np.sum(self.coefficients * np.array([self.K , self.lenght]))
 
-        print "Estimated TIME ", self.estimated_time
+        if self.samples >0:# not self.is_training:
+            print "Statistical Estimation " , self.estimated_time + self.lenght* self.mean_primitive_error/self.samples
+            if self.coefficients is not None:
+                estimated_time = np.sum(self.coefficients * np.array([self.K , self.estimated_time]))
+                print "Linearization Estimation " , estimated_time
+
 
     def MotionCompleteCB(self,msg):
         measured_time = (rospy.Time.now() - self.start_time).to_sec()
 
         if self.is_training:
             self.samples = self.samples+1
-            self.accumulator += (self.estimated_time - measured_time)/self.lenght
-            self.weight += (self.estimated_time - measured_time)
+            self.mean_primitive_error += (measured_time - self.estimated_time)/self.lenght
 
         print "MEASURED TIME", measured_time
         #print "ERROR IN SECONDS", self.estimated_time - measured_time
-        self.A.append(self.K)
-        self.y.append(self.estimated_time - measured_time)
+        self.A.append([self.K, self.estimated_time])
+        self.y.append(measured_time)
 
 
         fb_msgs = Vector3()
