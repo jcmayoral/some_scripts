7d4f2b8e69492c0830b2883de07ddee135b408f2
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2019-03-05 11:21:54 +0100

    satatable lidar to image

--- navigation/sensors_conversions/gazebo_3d_lidar_to_image.py
+++ navigation/sensors_conversions/gazebo_3d_lidar_to_image.py
@@ -4,77 +4,69 @@ import sensor_msgs.point_cloud2 as pc2
 from sensor_msgs.msg import PointCloud2, Image
 from numpy import fabs,sqrt, floor
 from numpy.linalg import norm
-global publisher
 
-def topic_cb(msg):
-    global last_data
-    gen = pc2.read_points(msg, skip_nans=False, field_names=("x", "y", "z","intensity" ))
-    transformed_image = Image()
-    transformed_image.header.frame_id = "velodyne"
-    x_resolution = 0.1
-    y_resolution = 0.1
-    ray_number = 800#int(3 / x_resolution)#220
-    size =  msg.height * msg.width
-    transformed_image.height = ray_number#fabs(floor(sqrt(size)))#
-    transformed_image.width = ray_number#fabs(floor(sqrt(size)))#
-    size =  int(transformed_image.height * transformed_image.width)
+class Lidar2Image:
+    def __init__(self):
+        rospy.init_node("lidar_to_image")
+        self.transformed_image = Image()
+        self.transformed_image.header.frame_id = "velodyne"
+        self.x_resolution = 0.05
+        self.y_resolution = 0.05
 
-    transformed_image.encoding = "mono8"
-    data = [0] * size
+        self.ray_number = 600
+        self.transformed_image.height = self.ray_number
+        self.transformed_image.width = self.ray_number
+        self.transformed_image.encoding = "mono8"
 
-    max_distance = 25 #max distance of lidar
-    min_distance = -3.5#-0.05 #for some reason is negative
-    scale_factor = 110
-    max_value = 255
+        self.max_distance = 20
+        self.max_value = 255
 
-    ignored_points =0
-    miscalculation = 0
+        self.size =  int(self.ray_number*self.ray_number)
+        self.publisher = rospy.Publisher("/scan_velodyne_hack/image_raw", Image, queue_size=1)
+        rospy.Subscriber("/velodyne_points", PointCloud2, self.topic_cb, queue_size=1)
+        rospy.loginfo("Node initialized")
+        rospy.spin()
 
-    for i in range(size-1, -1, -1):
-        try:
-            point = gen.next()
-            x = point[0]
-            y = point[1]
-            cell_x = round(x/x_resolution)
-            cell_y = round(y/y_resolution)
-            raw_value = point[3]/point[2]
-        except:
-            continue
-            print "NO"
 
-        value = raw_value
-        #value = min(fabs(raw_value / (max_distance- min_distance))*max_value, max_value)
-        #value = 100
-        #if raw_value > -1:
-        #    value = max_value
-        #value = 255
+    def topic_cb(self,msg):
+        gen = pc2.read_points(msg, skip_nans=False, field_names=("x", "y", "z","intensity" ))
 
-        cell_x = (transformed_image.height/2) + cell_x
 
-        if cell_x > transformed_image.height:
-            cell_x = cell_x - transformed_image.height
-            miscalculation_x = miscalculation+1
-            continue
+        data = [0] * self.size
 
-        cell_y = (transformed_image.width/2) + cell_y
+        for i in range(self.size-1, -1, -1):
+            try:
+                point = gen.next()
+                x = point[0]
+                y = point[1]
+                cell_x = round(x/self.x_resolution)
+                cell_y = round(y/self.y_resolution)
+                raw_value = point[3]/point[2]
+            except:
+                continue
+                print "NO"
 
-        if cell_y > transformed_image.width:
-            cell_y = cell_y - transformed_image.width
-            miscalculation_y = miscalculation+1
-            continue
+            if norm([x,y])> self.max_distance:
+                continue
 
-        index =  int(fabs(cell_x + transformed_image.width * cell_y))
+            cell_x = (self.ray_number/2) + cell_x
 
-        data[min(index, size -1)] = min(fabs(value), max_value)
+            if cell_x > self.ray_number:
+                continue
 
-        #data[i] = min(fabs(value), max_value)
-    print "total ignored points ", ignored_points
-    print "total miscalculation points  " , miscalculation_x, miscalculation_y
-    transformed_image.data = data
-    publisher.publish(transformed_image)
+            cell_y = (self.ray_number/2) + cell_y
 
-rospy.init_node("gazebo_3d_lidar_to_image")
+            if cell_y > self.ray_number:
+                continue
 
-publisher = rospy.Publisher("/scan_velodyne_hack/image_raw", Image, queue_size=1)
-rospy.Subscriber("/velodyne_points", PointCloud2, topic_cb, queue_size=1)
-rospy.spin()
+            index =  int(fabs(cell_x + self.ray_number * cell_y))
+
+            data[min(index, self.size -1)] = min(fabs(raw_value), self.max_value)
+
+        self.transformed_image.data = data
+        self.publisher.publish(self.transformed_image)
+
+
+
+if __name__ == '__main__':
+    Lidar2Image()
