82e4e7299d8abdbd3df499ccd088911d44e30121
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2019-04-25 16:52:32 +0200

    lidar2image updated

--- navigation/sensors_conversions/lidar2image_intensity.py
+++ navigation/sensors_conversions/lidar2image_intensity.py
@@ -10,10 +10,10 @@ class Lidar2Image:
         rospy.init_node("lidar_to_image")
         self.transformed_image = Image()
         self.transformed_image.header.frame_id = "velodyne"
-        self.x_resolution = 4
-        self.y_resolution = 4
+        self.x_resolution = 0.5
+        self.y_resolution = 0.5
 
-        self.pixels_number = 250
+        self.pixels_number = 200
         self.transformed_image.height = self.pixels_number
         self.transformed_image.width = self.pixels_number
         self.transformed_image.encoding = "mono8"
@@ -24,7 +24,7 @@ class Lidar2Image:
         self.transformed_image.data = [0] * self.size
 
         self.publisher = rospy.Publisher("/scan_velodyne_hack/image_raw", Image, queue_size=1)
-        rospy.Subscriber("/velodyne_points/filtered", PointCloud2, self.topic_cb, queue_size=1)
+        rospy.Subscriber("/velodyne_points", PointCloud2, self.topic_cb, queue_size=1)
         rospy.loginfo("Node initialized")
         rospy.spin()
 
@@ -32,30 +32,35 @@ class Lidar2Image:
     def topic_cb(self,msg):
         self.transformed_image.data = [0] * self.size
 
-        gen = pc2.read_points(msg, skip_nans=False, field_names=("x", "y", "z"))
+        gen = pc2.read_points(msg, skip_nans=True, field_names=("x", "y", "z","intensity" ))
 
         for i in range(self.size-1, -1, -1):
+            print "@"
             try:
                 point = gen.next()
                 x = point[0]
                 y = point[1]
                 z = point[2]
+                intensity = point[3]
                 cell_x = round(x*self.x_resolution)
                 cell_y = round(y*self.y_resolution)
-                feature = fabs(self.max_value * point[2])#*point[0]*point[1]
+                feature = point[3]/point[2]
             except:
+                print "error"
                 continue
 
             cell_x = (self.pixels_number/2) + cell_x
+            print cell_x
 
             #if cell_x > self.ray_number:
             #    continue
 
             cell_y = (self.pixels_number/2) + cell_y
+            print cell_y
             #if cell_y > self.ray_number:
             #    continue
 
-            index =  int(fabs(cell_x + self.transformed_image.height * cell_y))
+            index =  int(fabs(cell_x + self.pixels_number * cell_y))
             if self.size - index < 0:
                 print "error"
                 return
